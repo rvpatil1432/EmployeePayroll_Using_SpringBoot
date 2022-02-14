e2e5df0cb326b1acfe37f82113bf72f582efd406
rvpatil1432 <rvpatil1432@gmail.com>
2022-02-14 20:42:46 +0530

    Added : Create a Rest Controller to

---
+++ src/main/java/com/bridgelabz/employeepayrollapp/controller/EmployeePayrollController.java
@@ -0,0 +1,36 @@
+package com.bridgelabz.employeepayrollapp.controller;
+
+import com.bridgelabz.employeepayrollapp.dto.EmployeePayrollDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/employeepayrollservice")
+public class EmployeePayrollController {
+
+    @RequestMapping(value = {"","/","/get"})
+    public ResponseEntity<String> getEmployeePayrollData(){
+        return new ResponseEntity<>("Get call success", HttpStatus.OK);
+    }
+
+    @GetMapping("/get/{empId}")
+    public ResponseEntity<String> getEmployeePayrollData(@PathVariable("empId")int empId){
+        return new ResponseEntity<>("Get call success for id : "+empId, HttpStatus.OK);
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<String> addEmployeePayrollData(@RequestBody EmployeePayrollDTO employeePayrollDTO){
+        return new ResponseEntity<>("Created Employee Payroll Data for : "+employeePayrollDTO, HttpStatus.OK);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<String> updateEmployeePayrollData(@RequestBody EmployeePayrollDTO employeePayrollDTO){
+        return new ResponseEntity<>("Updated Employee Payroll Data for : "+employeePayrollDTO, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{empId}")
+    public ResponseEntity<String> deleteEmployeePayrollData(@PathVariable("empId") int empId){
+        return new ResponseEntity<>("delete call success for id : "+empId, HttpStatus.OK);
+    }
+}
