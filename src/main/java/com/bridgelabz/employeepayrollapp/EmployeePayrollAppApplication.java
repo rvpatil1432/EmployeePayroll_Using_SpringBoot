ceca738bb1b7322fb8c7de865c97f65b49f3cdd0
rvpatil1432 <rvpatil1432@gmail.com>
2022-02-17 20:34:11 +0530

    Added:Use case to set environment in dev and prod application properties file.

--- src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
@@ -2,6 +2,7 @@ package com.bridgelabz.employeepayrollapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -13,8 +14,8 @@ public class EmployeePayrollAppApplication {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Payroll App..");
-        SpringApplication.run(EmployeePayrollAppApplication.class, args);
-        log.info("Employee Payroll App Started.");
+        ApplicationContext context =  SpringApplication.run(EmployeePayrollAppApplication.class, args);
+        log.info("Employee Payroll App Started in {} Environment.",context.getEnvironment().getProperty("environment"));
     }
 
 }
