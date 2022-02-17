f94c9d52bfc536985c98b871e1d01bd89e99d9b0
rvpatil1432 <rvpatil1432@gmail.com>
2022-02-17 20:04:06 +0530

    Added:Use case to use lambok library logging to write log inside application.

--- src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
@@ -3,12 +3,18 @@ package com.bridgelabz.employeepayrollapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
+
+
 @SpringBootApplication
+@Slf4j
 public class EmployeePayrollAppApplication {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Payroll App..");
         SpringApplication.run(EmployeePayrollAppApplication.class, args);
+        log.info("Employee Payroll App Started.");
     }
 
 }
